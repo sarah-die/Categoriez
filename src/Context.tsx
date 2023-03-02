3f2c05b03212ec5584e58d1fbecb6f31d7482f09
sarah-die <saridi@alice.de>
2023-03-02 13:31:42 +0100

    Moved preset collections to a separate file.

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,66 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
 import { nanoid } from "nanoid";
-
-const presetCollections: Collection[] = [
-  {
-    name: "All",
-    id: "preset-collecction-all",
-    categoriez: [
-      "cat1.1",
-      "cat1.2",
-      "cta1.3",
-      "catloremsbnsid",
-      "catloremsbnsid",
-      "cat",
-      "cat",
-      "catloremsbnsid",
-    ],
-  },
-  {
-    name: "collection1",
-    id: "preset-collecction1",
-    categoriez: [
-      "cat1.1",
-      "cat1.2",
-      "cta1.3",
-      "cat4",
-      "catloremsbnsid",
-      "cat",
-      "catloremsbnsid",
-      "cat",
-      "cat",
-      "catloremsbnsid",
-      "catloremsbnsid",
-      "cat",
-      "cat",
-      "catloremsbnsid",
-    ],
-  },
-  {
-    name: "collection2",
-    id: "preset-collecction2",
-    categoriez: ["cat2.1", "catloremsbnsid.2", "cat", "cat", "cat", "cat"],
-  },
-  {
-    name: "collection3",
-    id: "preset-collecction3",
-    categoriez: [
-      "cat2.1",
-      "cat2.2",
-      "catloremsbnsid",
-      "catloremsbnsid",
-      "cat",
-      "cat",
-      "catloremsbnsid",
-      "cat",
-    ],
-  },
-  {
-    name: "collection4",
-    id: "preset-collecction4",
-    categoriez: ["cat2.1", "catloremsbnsid.2", "cat", "cat"],
-  },
-];
+import {presetCollections} from "./components/presetCollections";
 
 // globally available props
 type contextType = {
---
+++ src/components/presetCollections.tsx
@@ -0,0 +1,62 @@
+import { Collection } from "../Context";
+
+export const presetCollections: Collection[] = [
+  {
+    name: "All",
+    id: "preset-collecction-all",
+    categoriez: [
+      "cat1.1",
+      "cat1.2",
+      "cta1.3",
+      "catloremsbnsid",
+      "catloremsbnsid",
+      "cat",
+      "cat",
+      "catloremsbnsid",
+    ],
+  },
+  {
+    name: "collection1",
+    id: "preset-collecction1",
+    categoriez: [
+      "cat1.1",
+      "cat1.2",
+      "cta1.3",
+      "cat4",
+      "catloremsbnsid",
+      "cat",
+      "catloremsbnsid",
+      "cat",
+      "cat",
+      "catloremsbnsid",
+      "catloremsbnsid",
+      "cat",
+      "cat",
+      "catloremsbnsid",
+    ],
+  },
+  {
+    name: "collection2",
+    id: "preset-collecction2",
+    categoriez: ["cat2.1", "catloremsbnsid.2", "cat", "cat", "cat", "cat"],
+  },
+  {
+    name: "collection3",
+    id: "preset-collecction3",
+    categoriez: [
+      "cat2.1",
+      "cat2.2",
+      "catloremsbnsid",
+      "catloremsbnsid",
+      "cat",
+      "cat",
+      "catloremsbnsid",
+      "cat",
+    ],
+  },
+  {
+    name: "collection4",
+    id: "preset-collecction4",
+    categoriez: ["cat2.1", "catloremsbnsid.2", "cat", "cat"],
+  },
+];
