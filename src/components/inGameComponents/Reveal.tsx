4648df75a687db7736eec3cba085935aedaa0f78
sarah-die <saridi@alice.de>
2023-03-03 12:59:54 +0100

    Reveal adapted.
    Dependend on the current round another button is displayed - to either continue to the next round or end the game.

--- src/components/inGameComponents/Reveal.tsx
+++ src/components/inGameComponents/Reveal.tsx
@@ -1,8 +1,88 @@
 import Grid2 from "@mui/material/Unstable_Grid2";
+import { Button, Typography } from "@mui/material";
+import { useGameContext } from "../../Context";
+import { useResetGame } from "../utils/ResetGame";
 
 /** This component displays all categories from the current game. */
-export default  function Reveal() {
-    return (
-        <Grid2></Grid2>
-    );
+export default function Reveal() {
+  const ctx = useGameContext();
+  const resetGame = useResetGame();
+
+  const nextRound = () => {
+    const indexOf0: number = ctx.roundStatus.indexOf(0);
+    const newRoundStatus = [...ctx.roundStatus];
+    newRoundStatus.splice(indexOf0, 1, 1);
+    ctx.setInGameStatus("beginOfNewRound");
+  };
+
+  return (
+    <>
+      <Grid2
+        container
+        xs={12}
+        m={{ xs: 2, md: 9 }}
+        p={4}
+        sx={{ backgroundColor: "primary.dark", borderRadius: 2 }}
+        flexDirection={"column"}
+      >
+        <Typography color={"inherit"} variant={"body1"} my={1}>
+          Hier sind eure Kategorien. Nehmt euch nun die Zeit zu erraten wer
+          welche Kategorie gelegt hat. Wertet anschließend, vom Startspieler ({}
+          ) beginnend aus, ob ihr richtig getippt habt oder nicht. Verteilt
+          entsprechend Punkte mit Hilfe der Punkte-Chips.
+        </Typography>
+        <Grid2 container flexDirection={"row"} spacing={3}>
+          {ctx.assignedCategoriez.map((c, index) => {
+            return (
+              <Grid2 sm={6} md={4} lg={3} xl={2}>
+                <Typography p={1}>Nr. {index + 1}</Typography>
+                <Typography
+                  border={1}
+                  borderColor={"black"}
+                  borderRadius={2}
+                  px={2}
+                  py={1}
+                >
+                  {c}
+                </Typography>
+              </Grid2>
+            );
+          })}
+        </Grid2>
+      </Grid2>
+      <Grid2
+        container
+        xs={12}
+        mx={{ xs: 2, md: 9 }}
+        p={4}
+        sx={{ backgroundColor: "primary.dark", borderRadius: 2 }}
+        flexDirection={"column"}
+      >
+        <Typography color={"inherit"} variant={"body1"}>
+          Alle Punkte verteilt?
+        </Typography>
+        <Grid2 container justifyContent={"center"}>
+          {ctx.roundStatus.indexOf(0) + 1 === ctx.roundStatus.length ? (
+            <Button
+              variant={"contained"}
+              size={"large"}
+              sx={{ height: 50, fontSize: 22, my: 1, color: "black" }}
+              onClick={resetGame}
+            >
+              Spiel beenden
+            </Button>
+          ) : (
+            <Button
+              variant={"contained"}
+              size={"large"}
+              sx={{ height: 50, fontSize: 22, my: 1, color: "black" }}
+              onClick={nextRound}
+            >
+              Nächste Runde
+            </Button>
+          )}
+        </Grid2>
+      </Grid2>
+    </>
+  );
 }
