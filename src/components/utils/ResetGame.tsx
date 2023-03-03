c348740723b056530a81a96868b499b2d021a276
sarah-die <saridi@alice.de>
2023-03-03 12:58:05 +0100

    Edited ResetGame and renamed it to useResetGame. It now returns a useCallback.

--- src/components/utils/ResetGame.tsx
+++ src/components/utils/ResetGame.tsx
@@ -1,11 +1,17 @@
 import { initial, useGameContext } from "../../Context";
+import { useCallback } from "react";
 
-export const resetGame = () => {
+// Hooks können nicht in (TS)-Funktionen verwendet werden.
+// Aber es können eigene Hooks erstellt werden (beginnend mit use).
+// Hookifite Funktionen brauchen einen return mit useCallback und ein Dependency-Array.
+export const useResetGame = () => {
   const ctx = useGameContext();
-  ctx.setGameStatus(initial.gameStatus!);
-  ctx.setInGameStatus(initial.inGameStatus!);
-  ctx.setRoundStatus(initial.roundStatus!);
-  ctx.setAssignedCategoriez(initial.assignedCategoriez!);
-  ctx.setCategoriezWhitelist(initial.categoriezWhitelist!);
-  ctx.setChosenCollection(initial.chosenCollection!);
+  return useCallback(() => {
+    ctx.setGameStatus(initial.gameStatus!);
+    ctx.setInGameStatus(initial.inGameStatus!);
+    ctx.setRoundStatus(initial.roundStatus!);
+    ctx.setAssignedCategoriez(initial.assignedCategoriez!);
+    ctx.setCategoriezWhitelist(initial.categoriezWhitelist!);
+    ctx.setChosenCollection(initial.chosenCollection!);
+  }, [ctx]);
 };
