aea36505f50d48943714eb5b9f520476f0a23bb9
sarah-die <saridi@alice.de>
2023-03-24 15:45:25 +0100

    Changed the presetCollections.tsx

--- src/components/presetCollections2.tsx
+++
@@ -1,9 +0,0 @@
-import { Collection } from "../Context";
-
-export const presetCollections2: Collection[] = [
-  {
-    name: "Essen und Trinken",
-    id: "preset-collecction1",
-    categoriez: [],
-  },
-];
