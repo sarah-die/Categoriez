bd06bb6310cdc515513c5e8deabbdb7ccccfb9f2
sarah-die <saridi@alice.de>
2023-04-05 12:25:35 +0200

    When game is finished user is navigated to the main-Page

--- src/components/utils/useResetGame.ts
+++ src/components/utils/useResetGame.ts
@@ -1,10 +1,12 @@
 import { initial, useGameContext } from "../../Context";
 import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
 // Hooks können nicht in (TS)-Funktionen verwendet werden.
 // Aber es können eigene Hooks erstellt werden (beginnend mit use).
 // Hookifite Funktionen brauchen einen return mit useCallback und ein Dependency-Array.
 export const useResetGame = () => {
+  const navigate = useNavigate();
   const ctx = useGameContext();
   return useCallback(() => {
     ctx.setGameStatus(initial.gameStatus!);
@@ -13,5 +15,6 @@ export const useResetGame = () => {
     ctx.setAssignedCategoriez(initial.assignedCategoriez!);
     ctx.setCategoriezWhitelist(initial.categoriezWhitelist!);
     ctx.setChosenCollections(initial.chosenCollections!);
-  }, [ctx]);
+    navigate("/");
+  }, [ctx, navigate]);
 };
